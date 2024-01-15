9603d368569e42306be1ffa29e6839f982a29a64
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-01-15 20:02:31 +0600

    some modified

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hasduihadguwgiuwd654'
 
-    # Additional setup and configuration here
+
+    from .views import views
+    from .auth import auth
 
     return app
